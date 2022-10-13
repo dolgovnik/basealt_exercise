f6d62d5b3b037fcc032cbf17234cb7397a3be59f
Nikolay Dolgov <dolgovnik@gmail.com>
2022-10-13 22:47:35 +0300

    basealt.py: 1. Change method names to not public: ask_api -> _ask_api,    get_packages_for_arch -> _get_packages_for_arch 2. Create new set to keep archs names, change self.archs to self.row_archs 3. Create packages_sets dict to keep data for future comparing 4. Add __repr__ method

--- basealt.py
+++ basealt.py
@@ -12,16 +12,25 @@ class Branch:
         self.branch = branch
         ioloop = asyncio.get_event_loop()
         # Get all branch archs
-        self.archs = ioloop.run_until_complete(self.ask_api(self.URL_ALL_PKGSET_ARCHS))
-        if 'errors' in self.archs:
-            raise Exception(self.archs['errors'])
+        self.row_archs = ioloop.run_until_complete(self._ask_api(self.URL_ALL_PKGSET_ARCHS))
+        if 'errors' in self.row_archs:
+            raise Exception(self.row_archs['errors'])
+
+        self.archs_set = {i['arch'] for i in self.row_archs['archs']}
 
         # Get all packages for branch arch
-        self.arch_packages_data = ioloop.run_until_complete(self.get_packages_for_arch())
+        self.raw_packages = ioloop.run_until_complete(self._get_packages_for_arch())
 
+        # Fill package sets
+        self.packages_sets = {}
+        for arch in self.raw_packages:
+            if 'errors' in arch:
+                continue
+            self.packages_sets[arch['request_args']['arch']] = {'packages_set': {i['name'] for i in arch['packages']},
+                                                                'packages': {i['name']: i for i in arch['packages']}}
         # ioloop.close()
 
-    async def ask_api(self, URL: str, params: dict = {}):
+    async def _ask_api(self, URL: str, params: dict = {}):
         req_url = f'{URL}{self.branch}'
         if params:
             req_url = f'{req_url}?'
@@ -31,10 +40,13 @@ class Branch:
             async with session.get(req_url) as resp:
                 return json.loads(await resp.text())
 
-    async def get_packages_for_arch(self):
+    async def _get_packages_for_arch(self):
         arch_packages_data = []
-        futures = [self.ask_api(self.URL_BRANCH_BINARY_PACKAGES, {'arch': i['arch']}) for i in self.archs['archs']]
+        futures = [self._ask_api(self.URL_BRANCH_BINARY_PACKAGES, {'arch': i}) for i in self.archs_set]
         for d in asyncio.as_completed(futures):
             arch_packages_data.append(await d)
 
         return arch_packages_data
+
+    def __repr__(self):
+        return f'Branch(\'{self.branch}\')'
