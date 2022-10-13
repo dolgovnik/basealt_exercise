7706ae7e3e36fec12b8a2ee8c39155f15e549526
Nikolay Dolgov <dolgovnik@gmail.com>
2022-10-14 00:22:10 +0300

    basealt.py: 1. Fix typo: row_archs -> raw_archs 2. Add some annotations to methods 3. Add compare method, which compares two Branch objects 4. Add compare_results_as_json method, which returns compare result as    json

--- basealt.py
+++ basealt.py
@@ -2,21 +2,24 @@ import asyncio
 import aiohttp
 import json
 
+from distutils.version import LooseVersion
+
 
 class Branch:
 
     URL_BRANCH_BINARY_PACKAGES = 'https://rdb.altlinux.org/api/export/branch_binary_packages/'
     URL_ALL_PKGSET_ARCHS = 'https://rdb.altlinux.org/api/site/all_pkgset_archs?branch='
 
-    def __init__(self, branch: str):
+    def __init__(self, branch: str) -> None:
         self.branch = branch
+        self.comparsion_result = None
         ioloop = asyncio.get_event_loop()
         # Get all branch archs
-        self.row_archs = ioloop.run_until_complete(self._ask_api(self.URL_ALL_PKGSET_ARCHS))
-        if 'errors' in self.row_archs:
-            raise Exception(self.row_archs['errors'])
+        self.raw_archs = ioloop.run_until_complete(self._ask_api(self.URL_ALL_PKGSET_ARCHS))
+        if 'errors' in self.raw_archs:
+            raise Exception(self.raw_archs['errors'])
 
-        self.archs_set = {i['arch'] for i in self.row_archs['archs']}
+        self.archs_set = {i['arch'] for i in self.raw_archs['archs']}
 
         # Get all packages for branch arch
         self.raw_packages = ioloop.run_until_complete(self._get_packages_for_arch())
@@ -30,7 +33,7 @@ class Branch:
                                                                 'packages': {i['name']: i for i in arch['packages']}}
         # ioloop.close()
 
-    async def _ask_api(self, URL: str, params: dict = {}):
+    async def _ask_api(self, URL: str, params: dict = {}) -> dict:
         req_url = f'{URL}{self.branch}'
         if params:
             req_url = f'{req_url}?'
@@ -40,13 +43,48 @@ class Branch:
             async with session.get(req_url) as resp:
                 return json.loads(await resp.text())
 
-    async def _get_packages_for_arch(self):
+    async def _get_packages_for_arch(self) -> list:
         arch_packages_data = []
         futures = [self._ask_api(self.URL_BRANCH_BINARY_PACKAGES, {'arch': i}) for i in self.archs_set]
         for d in asyncio.as_completed(futures):
             arch_packages_data.append(await d)
 
         return arch_packages_data
+
+    def compare(self, other: 'Branch') -> None:
+        self.comparsion_result = {'compared_to_branch': other.branch}
+
+        for arch, data in self.packages_sets.items():
+            self.comparsion_result[arch] = {}
+            added_names = list(data['packages_set'] - other.packages_sets[arch]['packages_set'])
+            added_names.sort()
+            added = []
+            for p in added_names:
+                added.append((p, data['packages'][p]['version']))
+            removed_names = list(other.packages_sets[arch]['packages_set'] - data['packages_set'])
+            removed_names.sort()
+            removed = []
+            for p in removed_names:
+                removed.append((p, other.packages_sets[arch]['packages'][p]['version']))
+
+            self.comparsion_result[arch]['added'] = added
+            self.comparsion_result[arch]['removed'] = removed
+
+            intersected = list(data['packages_set'].intersection(other.packages_sets[arch]['packages_set']))
+            intersected.sort()
+            updated = []
+            suspicious = []
+            for p in intersected:
+                try:
+                    if LooseVersion(data['packages'][p]['version']) > LooseVersion(other.packages_sets[arch]['packages'][p]['version']):
+                        updated.append((p, data['packages'][p]['version'], other.packages_sets[arch]['packages'][p]['version']))
+                except TypeError:
+                    suspicious.append((p, data['packages'][p]['version'], other.packages_sets[arch]['packages'][p]['version']))
+            self.comparsion_result[arch]['updated'] = updated
+            self.comparsion_result[arch]['suspicious'] = suspicious
+
+    def compare_results_as_json(self) -> str:
+        return json.dumps(self.comparsion_result)
 
     def __repr__(self):
         return f'Branch(\'{self.branch}\')'
