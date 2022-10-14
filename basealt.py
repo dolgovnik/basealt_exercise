6562bd49144c1299b35e03897f9be5cea91cf756
Nikolay Dolgov <dolgovnik@gmail.com>
2022-10-14 22:31:07 +0300

    basealt.py and basealt_cli.py: 1. Add comments

--- basealt.py
+++ basealt.py
@@ -6,6 +6,9 @@ from distutils.version import LooseVersion
 
 
 class Branch:
+    '''
+    Branch class represents binary packages for branch in Simply Linux repository
+    '''
 
     URL_BRANCH_BINARY_PACKAGES = 'https://rdb.altlinux.org/api/export/branch_binary_packages/'
     URL_ALL_PKGSET_ARCHS = 'https://rdb.altlinux.org/api/site/all_pkgset_archs?branch='
@@ -13,27 +16,32 @@ class Branch:
     def __init__(self, branch: str) -> None:
         self.branch = branch
         self.comparsion_result = None
+        # Event loop for async http requests
         ioloop = asyncio.get_event_loop()
-        # Get all branch archs
+        # Get all branch archetectures
         self.raw_archs = ioloop.run_until_complete(self._ask_api(self.URL_ALL_PKGSET_ARCHS))
         if 'errors' in self.raw_archs:
             raise Exception(self.raw_archs['errors'])
 
+        # List of archetectures for future use
         self.archs_set = [i['arch'] for i in self.raw_archs['archs']]
 
-        # Get all packages for branch arch
+        # Get all packages for branch arch via event loop
         self.raw_packages = ioloop.run_until_complete(self._get_packages_for_arch())
 
-        # Fill package sets
+        # Fill package sets for future comparsion
         self.packages_sets = {}
         for arch in self.raw_packages:
             if 'errors' in arch:
                 continue
             self.packages_sets[arch['request_args']['arch']] = {'packages_set': {i['name'] for i in arch['packages']},
                                                                 'packages': {i['name']: i for i in arch['packages']}}
-        # ioloop.close()
+        ioloop.close()
 
     async def _ask_api(self, URL: str, params: dict = {}) -> dict:
+        '''
+        Send async HTTP GET request to API
+        '''
         req_url = f'{URL}{self.branch}'
         if params:
             req_url = f'{req_url}?'
@@ -44,6 +52,9 @@ class Branch:
                 return json.loads(await resp.text())
 
     async def _get_packages_for_arch(self) -> list:
+        '''
+        Prepare _ask_api Futures list to run it in event loop
+        '''
         arch_packages_data = []
         futures = [self._ask_api(self.URL_BRANCH_BINARY_PACKAGES, {'arch': i}) for i in self.archs_set]
         for d in asyncio.as_completed(futures):
@@ -53,21 +64,32 @@ class Branch:
 
     @staticmethod
     def _prepare_list(r_set: set, data: list) -> list:
+        '''
+        Static method to create part of comparsion result structure
+        '''
         lst = list(r_set)
         lst.sort()
         return [(p, data['packages'][p]['version']) for p in lst]
 
     def compare(self, other: 'Branch') -> None:
+        '''
+        Method compares current Branch oject with other one
+        '''
+        # Data structure for comparsion result
         self.comparsion_result = {'current_branch': self.branch, 'compared_to_branch': other.branch, 'result': {}}
 
         for arch, data in self.packages_sets.items():
+            # Each archetecture in own dict
             self.comparsion_result['result'][arch] = {}
+            # Packages added to current branch compared to other branch
             added_names = data['packages_set'] - other.packages_sets[arch]['packages_set']
             self.comparsion_result['result'][arch]['added'] = self._prepare_list(added_names, data)
 
+            # Packages removed from current branch compared to other branch
             removed_names = other.packages_sets[arch]['packages_set'] - data['packages_set']
             self.comparsion_result['result'][arch]['removed'] = self._prepare_list(removed_names, other.packages_sets[arch])
 
+            # Packages updated in current branch compared to other branch
             intersected = list(data['packages_set'].intersection(other.packages_sets[arch]['packages_set']))
             intersected.sort()
             updated = []
@@ -77,11 +99,15 @@ class Branch:
                     if LooseVersion(data['packages'][p]['version']) > LooseVersion(other.packages_sets[arch]['packages'][p]['version']):
                         updated.append((p, data['packages'][p]['version'], other.packages_sets[arch]['packages'][p]['version']))
                 except TypeError:
+                    # Packages which versions impossible to compare
                     suspicious.append((p, data['packages'][p]['version'], other.packages_sets[arch]['packages'][p]['version']))
             self.comparsion_result['result'][arch]['updated'] = updated
             self.comparsion_result['result'][arch]['suspicious'] = suspicious
 
     def compare_results_as_json(self) -> str:
+        '''
+        Return comparsion results as json
+        '''
         return json.dumps(self.comparsion_result)
 
     def __repr__(self):
--- basealt_cli.py
+++ basealt_cli.py
@@ -3,8 +3,25 @@ import json
 
 from basealt import Branch
 
+'''
+CLI utility to work with Branch objects. It compares two branch objects and filters result.
+
+Examples.
+Get all archetectures for branch p10:
+python3 basealt_cli.py getarchs p10
+
+Compare packages in two branches sisyphus and p10:
+python3 basealt_cli.py compare sisyphus p10
+
+
+Compare packages in two branches sisyphus and p10, but show results for x86_64 and noarch archetectures
+and only added and updated packages:
+python3 basealt_cli.py compare sisyphus p10 -t added updated -a x86_64 noarch
+'''
+
 if __name__ == '__main__':
 
+    # Parse CLI arguments
     parser = argparse.ArgumentParser(description='Comparing binary packages between two branches of Simply Linux')
 
     parser.add_argument('cmd', type=str, choices=['getarchs', 'compare'],
@@ -18,11 +35,14 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    # Get avaliable archetectures for branch
     if args.cmd == 'getarchs':
         b1 = Branch(args.branch[0])
         print('\nAvaliable archetectures are:')
         print(json.dumps(b1.archs_set, indent=4))
+    # Compare packages in two branches
     elif args.cmd == 'compare':
+        # If only one branch in input - show error message
         if len(args.branch) < 2:
             print('There should be two branches to compare')
         else:
@@ -31,6 +51,7 @@ if __name__ == '__main__':
             b1.compare(b2)
             result = b1.comparsion_result
 
+            # If archetecture or type in input - filter result dict
             if args.arch:
                 archs_to_del = set(result['result'].keys()) - set(args.arch)
                 for a in archs_to_del:
@@ -42,4 +63,5 @@ if __name__ == '__main__':
                     for a in types_to_del:
                         del result['result'][arch][a]
 
+            # Prettyprint json result
             print(json.dumps(result, indent=4))
